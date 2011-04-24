240280e28123ee97d53d401aeca084fe5a91a229
Christian Herlambang <christian.herlambang@gmail.com>
2011-04-24 17:40:48 +0200

    created readme

--- sse-server.js
+++ sse-server.js
@@ -3,21 +3,22 @@ var sys = require('sys');
 var fs = require('fs');
 
 http.createServer(
-        function(req, res) {
-            debugHeaders(req);
-            if (req.headers.accept && req.headers.accept == 'text/event-stream') {
-                if (req.url == '/events') {
-                    sendSSE(req, res);
-                } else {
-                    res.writeHead(404);
-                    res.end();
-                }
+    function(req, res) {
+        debugHeaders(req);
+        if (req.headers.accept && req.headers.accept == 'text/event-stream') {
+            if (req.url == '/events') {
+                sendSSE(req, res);
             } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(fs.readFileSync(__dirname + '/sse.html'));
+                res.writeHead(404);
                 res.end();
             }
-        }).listen(8000);
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(fs.readFileSync(__dirname + '/sse.html'));
+            res.end();
+        }
+}).listen(8000);
+console.log("Server running at http://localhost:8000");
 
 function debugHeaders(req) {
     sys.puts('URL Path: ' + req.url);
@@ -29,18 +30,18 @@ function debugHeaders(req) {
 
 function sendSSE(req, res) {
     res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive'
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive'
     });
     var id = (new Date()).toLocaleTimeString();
     setInterval(function() {
         constructSSE(res, id, (new Date()).toLocaleTimeString());
     }, 1000);
-}
+    }
 
-function constructSSE(res, id, data) {
-    res.write('id: ' + id + '\n');
-    res.write("data: " + data + '\n\n');
-}
+    function constructSSE(res, id, data) {
+        res.write('id: ' + id + '\n');
+        res.write("data: " + data + '\n\n');
+    }
 
